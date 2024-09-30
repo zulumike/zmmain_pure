ea62471a9f7fed06fd4e6997330e0f6ef244ae72
zulumike <ole@zmmaskin.no>
2024-09-30 15:55:01 +0200

    new customer form

--- customers/customerlist.js
+++ customers/customerlist.js
@@ -3,13 +3,16 @@ import { readAllCustomers } from "/scripts/dbfunctions.js";
 async function getCustomerList() {
     const customerList = await readAllCustomers();
     console.log(customerList);
-    const table = document.getElementById("customer-table");
+    const table = document.getElementById("cust-table-body");
     for (let i = 0; i < customerList.length; i++) {
         const newRow = table.insertRow(-1);
         const custNr = newRow.insertCell(0);
         const custName = newRow.insertCell(1);
         custNr.innerText = customerList[i].nr;
         custName.innerText = customerList[i].name;
+        newRow.addEventListener('click', () => {
+            window.location.href = 'newcustomer.html';
+        })
     }
 }
 
