039735221d80dfb64033f551259bca63448d9ffa
zulumike <ole@zmmaskin.no>
2024-10-10 22:57:55 +0200

    Orderlines

--- orders/editorder.js
+++ orders/editorder.js
@@ -1,21 +1,30 @@
 import { updateDocument, getDocument, deleteDocument, readAllCustomers, readAllProducts } from "./dbfunctions.js";
 import { loaderOn, loaderOff } from "../scripts/functions.js";
 
+let orderData = {};
+
 const documentForm = document.getElementById("documentform");
 documentForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     loaderOn();
     const documentFormData = new FormData(documentForm);
     const documentData = Object.fromEntries(documentFormData);
+    documentData.customer = parseInt(documentData.customer);
+    documentData.orderLines = orderData.orderLines;
     await updateDocument(documentId, documentData);
     loaderOff();
-    window.location.replace('/customers/customerlist.html');
+    window.location.replace('/orders/orderlist.html');
 })
 
 function presentOrderLines(orderLines) {
+    const existingTable = document.getElementById('orderlinetable');
+    if (existingTable != null) {
+        existingTable.remove();
+    }
     const orderLineDiv = document.getElementById('orderlinediv');
     const olTable = document.createElement('table');
     olTable.classList = ['list-table'];
+    olTable.id = 'orderlinetable';
     const tableHeading = document.createElement('thead');
     const tableBody = document.createElement('tbody');
     olTable.appendChild(tableHeading);    
@@ -36,7 +45,6 @@ function presentOrderLines(orderLines) {
     hCell6.innerText = 'Beløp:';
     const hCell7 = headingRow.insertCell(6);
     hCell7.innerText = 'Kommentar:';
-    console.table(orderLines);
     for (let i = 0; i < orderLines.length; i++) {
         const bodyRow = tableBody.insertRow(-1);
         const date = bodyRow.insertCell(0);
@@ -51,9 +59,22 @@ function presentOrderLines(orderLines) {
         price.innerText = orderLines[i].price;
         amount.innerText = orderLines[i].amount;
         unit.innerText = orderLines[i].unit;
-        sum.innerText = orderLines[i].sum;
+        sum.innerText = orderLines[i].price * orderLines[i].amount;
         comment.innerText = orderLines[i].comment;
+        bodyRow.addEventListener('click', () => {
+            populateOrderLineForm(orderLines[i]);
+        });
     }
+}
+function addOrderLine(event, orderLineForm) {
+    event.preventDefault();
+    const orderLineFormData = new FormData(orderLineForm);
+    const orderLineData = Object.fromEntries(orderLineFormData);
+    orderLineData.price = parseFloat(orderLineData.price);
+    orderLineData.amount = parseFloat(orderLineData.amount);
+    orderData.orderLines.push(orderLineData);
+    presentOrderLines(orderData.orderLines);
+    console.table(orderLineData);
 }
 
 async function orderLineForm() {
@@ -67,7 +88,7 @@ async function orderLineForm() {
     const emptyOLDate = document.createElement('input');
     emptyOLDate.type = 'date';
     emptyOLDate.id = 'oldate';
-    emptyOLDate.name = 'oldate';
+    emptyOLDate.name = 'date';
     const today = new Date();
     emptyOLDate.valueAsDate = today;
     dateDiv.appendChild(dateLabel);
@@ -79,7 +100,7 @@ async function orderLineForm() {
     productIdLabel.innerText = 'Produkt';
     const emptyOLProductId = document.createElement('select');
     emptyOLProductId.id = 'olproductid';
-    emptyOLProductId.name = 'olproductid';
+    emptyOLProductId.name = 'product';
     const blankProduct = document.createElement('option');
     blankProduct.textContent = 'Velg produkt';
     emptyOLProductId.appendChild(blankProduct);
@@ -101,7 +122,7 @@ async function orderLineForm() {
     const emptyOLAmount = document.createElement('input');
     emptyOLAmount.type = 'number';
     emptyOLAmount.id = 'olamount';
-    emptyOLAmount.name = 'olamount';
+    emptyOLAmount.name = 'amount';
     amountDiv.appendChild(amountLabel);
     amountDiv.appendChild(emptyOLAmount);
 
@@ -112,7 +133,7 @@ async function orderLineForm() {
     const emptyOLUnit = document.createElement('input');
     emptyOLUnit.type = 'text';
     emptyOLUnit.id = 'olunit';
-    emptyOLUnit.name = 'olunit';
+    emptyOLUnit.name = 'unit';
     unitDiv.appendChild(unitLabel);
     unitDiv.appendChild(emptyOLUnit);
 
@@ -123,46 +144,58 @@ async function orderLineForm() {
     const emptyOLPrice = document.createElement('input');
     emptyOLPrice.type = 'number';
     emptyOLPrice.id = 'olprice';
-    emptyOLPrice.name = 'olprice';
+    emptyOLPrice.name = 'price';
     priceDiv.appendChild(priceLabel);
     priceDiv.appendChild(emptyOLPrice);
-
-    const sumDiv = document.createElement('div');
-    const sumLabel = document.createElement('label');
-    sumLabel.for = 'olsum';
-    sumLabel.innerText = 'Beløp';
-    const emptyOLSum = document.createElement('input');
-    emptyOLSum.type = 'number';
-    emptyOLSum.id = 'olsum';
-    emptyOLSum.name = 'olsum';
-    sumDiv.appendChild(sumLabel);
-    sumDiv.appendChild(emptyOLSum);
 
     const commentDiv = document.createElement('div');
     const divLabel = document.createElement('label');
     divLabel.for = 'olcomment';
     divLabel.innerText = 'Kommentar';
     const emptyOLComment = document.createElement('input');
-    emptyOLComment.type = 'number';
+    emptyOLComment.type = 'text';
     emptyOLComment.id = 'olcomment';
-    emptyOLComment.name = 'olcomment';
+    emptyOLComment.name = 'comment';
     commentDiv.appendChild(divLabel);
     commentDiv.appendChild(emptyOLComment);
 
+    const submitOrderLine = document.createElement('input');
+    submitOrderLine.type = 'submit';
+    submitOrderLine.value = 'Legg til';
+    
+    orderLineForm.addEventListener('submit', (event) => {
+        addOrderLine(event, orderLineForm);
+        orderLineForm.reset();
+    })
+
+    const resetOrderLineForm = document.createElement('input');
+    resetOrderLineForm.type = 'reset';
+    
     orderLineForm.appendChild(dateDiv);
     orderLineForm.appendChild(productDiv);
     orderLineForm.appendChild(priceDiv);
     orderLineForm.appendChild(amountDiv);
     orderLineForm.appendChild(unitDiv);
-    orderLineForm.appendChild(sumDiv);
     orderLineForm.appendChild(commentDiv);
+    orderLineForm.appendChild(submitOrderLine);
+    orderLineForm.appendChild(resetOrderLineForm);
+}
+
+function populateOrderLineForm(orderLineData) {
+    document.getElementById('oldate').value = orderLineData.date;
+    document.getElementById('olproductid').value = orderLineData.product;
+    document.getElementById('olprice').value = orderLineData.price;
+    document.getElementById('olamount').value = orderLineData.amount;
+    document.getElementById('olunit').value = orderLineData.unit;
+    document.getElementById('olcomment').value = orderLineData.comment;
+    console.log(typeof(document.getElementById('olproductid').value));
 }
 
 async function populatedocumentForm(documentId) {
     loaderOn();
-    const documentData = await getDocument(documentId)
-    document.getElementById('formheading').textContent = 'Ordrenr: ' + documentData.id;
-    document.getElementById('customer').value = documentData.customer;
+    orderData = await getDocument(documentId)
+    document.getElementById('formheading').textContent = 'Ordrenr: ' + orderData.id;
+    document.getElementById('customer').value = orderData.customer;
     const customerList = await readAllCustomers();
     const customerSelect = document.getElementById('customer');
     for (let i = 0; i < customerList.length; i++) {
@@ -170,31 +203,32 @@ async function populatedocumentForm(documentId) {
             const customerOption = document.createElement('option');
             customerOption.textContent = customerList[i].name;
             customerOption.value = customerList[i].id;
-            if (documentData.customer === customerList[i].id) {
-                customerOption.selected;
+            if (orderData.customer === parseInt(customerList[i].id)) {
+                customerOption.selected = true;
             }
             customerSelect.appendChild(customerOption);
         }
     }
-    document.getElementById('name').value = documentData.name;
-    document.getElementById('date').value = documentData.date;
+    document.getElementById('name').value = orderData.name;
+    document.getElementById('date').value = orderData.date;
     const createdText = document.getElementById('created-text');
     const updatedText = document.getElementById('updated-text');
-    const createdTime = new Date(documentData.created);
+    const createdTime = new Date(orderData.created);
     createdText.textContent = 'Opprettet: ' + createdTime.toLocaleString() +
-        ' av ' + documentData.created_by;
-    if (documentData.updated != null) {
-        const updatedTime = new Date(documentData.updated);
+        ' av ' + orderData.created_by;
+    if (orderData.updated != null) {
+        const updatedTime = new Date(orderData.updated);
         updatedText.textContent = 'Sist oppdatert: ' + updatedTime.toLocaleString() + 
-            ' av ' + documentData.updated_by;
+            ' av ' + orderData.updated_by;
     }
     else {
         updatedText.remove();
     }
     orderLineForm();
-    if (documentData.orderLines.length > 0) {
-        presentOrderLines(documentData.orderLines);
+    if (orderData.orderLines.length > 0) {
+        presentOrderLines(orderData.orderLines);
     }
+  
     loaderOff();
 }
 
