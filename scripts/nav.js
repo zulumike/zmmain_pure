3a00af795220dc67010aac642574b7c78b9d4192
zulumike <ole@zmmaskin.no>
2024-10-11 21:56:50 +0200

    Moved navbar from every html to template in nav.js

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,23 @@
+const navTemplate = document.createElement('template');
+navTemplate.innerHTML = `
+    <ul class="nav-links">
+        <li id="navhome"><a href="/">Hjem</a></li>
+        <li id="navcustomers"><a href="/customers/customerlist.html">Kunder</a></li>
+        <li id="navproducts"><a href="/products/productlist.html">Produkter</a></li>
+        <li id="navorders"><a href="/orders/orderlist.html">Ordrer</a></li>
+        <li id="navinvoices"><a href="/invoices/invoicelist.html">Faktura</a></li>
+        <li id="navcosts"><a href="/costs/costlist.html">Billag</a></li>
+        <li id="navadmin"><a href="/admin/admin.html">Admin</a></li>
+    </ul>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
+`
+const navBar = document.getElementById('navbar');
+navBar.appendChild(navTemplate.content);
+
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
 const body = document.querySelector('body');
